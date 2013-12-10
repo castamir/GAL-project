8ff22db48d28310541da9a990d4d696e43584a70
nesty <igor.pavlu@seznam.cz>
2013-12-10 12:22:37 +0100

    pseudoalgoritmus detekce smycky

---
+++ core.py
@@ -0,0 +1,33 @@
+import gal.py
+
+# dfs (graf(V,H))
+# for u in V
+#   color[u] = white
+#   predek = NULL
+# for u in V
+#   if color[u] == white
+#      dfs_visit(u)
+
+# dfs_visit(u)
+#   color = gray
+#   for v in adj[u]
+#     if color[v]  == white
+#       predek += u
+#       dfs_visit(v)
+#     else
+#       pole.add(v)
+#       find_cycle(v, v)
+#   color[u] = black
+#
+# find_cycle(u, v)
+#  cycles = 0
+#  pole.add(u)
+#  for p in predek[u]
+#    if(p == v) { # detekovana smycka
+#       cycles++
+#       prekopiruj pole do cilove matice smycek
+#    }
+#    else if (cycles = find_cycle(p,v))
+#    if (cycles == 0)# nenasla se smycka
+#
+#  return cycles
