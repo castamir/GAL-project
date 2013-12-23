704f97a29d8de2d75bd2d9776e757c78d409fba7
castamir <miras.paulik@email.cz>
2013-12-23 20:17:57 +0100

    typos

--- gui.py
+++ gui.py
@@ -4,6 +4,9 @@
 from Tkinter import Tk, Text, BOTH, W, N, E, S, Canvas, Menu, END
 from ttk import Frame, Button, Label, Style
 import tkFileDialog
+import tkMessageBox as box
+from pygraphml.GraphMLParser import *
+from pygraphml.Graph import *
 
 
 class Example(Frame):
@@ -19,46 +22,51 @@ class Example(Frame):
 
         self.columnconfigure(1, weight=1)
         self.columnconfigure(3, pad=7)
-        self.rowconfigure(3, weight=1)
+        self.rowconfigure(4, weight=1)
         self.rowconfigure(5, pad=7)
 
-        self.area = Canvas(self)
-        self.area.grid(row=1, column=0, columnspan=2, rowspan=4,
-                       pady=3,
-                       padx=5, sticky=E + W + S + N)
+        self.label = Label(self, text="graf1.graphml")
+        self.label.grid(sticky=W, pady=4, padx=5)
 
-        self.abtn = Button(self, text="Activate")
-        self.abtn.grid(row=1, column=3)
+        self.canvas = Canvas(self)
+        self.canvas.grid(row=1, column=0, columnspan=2, rowspan=5,
+                         padx=5, sticky=E + W + S + N)
 
-        self.cbtn = Button(self, text="Close")
-        self.cbtn.grid(row=2, column=3, pady=4)
+        abtn = Button(self, text="Vyhledat cykly")
+        abtn.grid(row=1, column=3)
 
-        self.hbtn = Button(self, text="Help")
-        self.hbtn.grid(row=5, column=0, padx=5)
+        cbtn = Button(self, text=">>")
+        cbtn.grid(row=2, column=3, pady=4)
 
-        self.obtn = Button(self, text="OK")
-        self.obtn.grid(row=5, column=3)
+        cbtn = Button(self, text="<<")
+        cbtn.grid(row=3, column=3, pady=4)
+
+        #hbtn = Button(self, text="Help")
+        #hbtn.grid(row=5, column=0, padx=5)
+        #
+        obtn = Button(self, text="Reset")
+        obtn.grid(row=5, column=3)
 
         menubar = Menu(self.parent)
         self.parent.config(menu=menubar)
 
         fileMenu = Menu(menubar)
-        fileMenu.add_command(label="Load", command=self.onLoad)
-        fileMenu.add_command(label="Save", command=self.onSave)
+        fileMenu.add_command(label="Načíst", command=self.onLoad)
+        fileMenu.add_command(label="Uložit", command=self.onSave)
         fileMenu.add_separator()
-        fileMenu.add_command(label="Exit", command=self.onExit)
-        menubar.add_cascade(label="File", menu=fileMenu)
+        fileMenu.add_command(label="Konec", command=self.onExit)
+        menubar.add_cascade(label="Soubor", menu=fileMenu)
 
         fileMenu = Menu(menubar)
-        fileMenu.add_command(label="About", command=self.onAbout)
-        menubar.add_cascade(label="Help", menu=fileMenu)
+        fileMenu.add_command(label="O aplikaci", command=self.onAbout)
+        menubar.add_cascade(label="Nápověda", menu=fileMenu)
 
 
     def onExit(self):
         self.quit()
 
     def onLoad(self):
-        fileTypes = [('GraphML files', '*.xml')]
+        fileTypes = [('Soubory typu GraphML', '*.graphml')]
 
         dialog = tkFileDialog.Open(self, filetypes=fileTypes)
         filename = dialog.show()
@@ -69,14 +77,48 @@ class Example(Frame):
             print text
 
     def onSave(self):
-        pass
+        fileTypes = [('GraphML files', '*.graphml')]
+
+        dialog = tkFileDialog.SaveAs(self, filetypes=fileTypes)
+        filename = dialog.show()
+
+        if not filename.endswith(".graphml"):
+            filename += ".graphml"
+        print filename
+
+        self.writeFile(filename)
 
 
     def onAbout(self):
-        pass
+        box.showinfo("O aplikaci",
+                     "Demonstrace algoritmu nalezení elementárních cyklů v orientovaném grafu podle D. B. Johnsona. \n\n"
+                     "Autoři:\n"
+                     "Paulík Miroslav\n"
+                     "Pavlů Igor\n"
+                     "FIT VUT v Brně 2013")
 
 
     def readFile(self, filename):
         f = open(filename, "r")
         text = f.read()
         return text
+
+
+    def writeFile(self, filename):
+        g = Graph()
+
+        n1 = g.add_node("A")
+        n2 = g.add_node("B")
+        n3 = g.add_node("C")
+        n4 = g.add_node("D")
+        n5 = g.add_node("E")
+        n5['x'] = 5
+        n5['y'] = 199
+
+        g.add_edge(n1, n3)
+        g.add_edge(n2, n3)
+        g.add_edge(n3, n4)
+        g.add_edge(n3, n5)
+
+        parser = GraphMLParser()
+        parser.write(g, filename)
